f7fe797bb0209655f629a56309d04c44792f1759
yingxuezhang <35289516+yingxuezhang@users.noreply.github.com>
2018-07-04 22:14:05 +0800

    Create 347.java

---
+++ 347.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+        List<Integer>[] bucket = new List[nums.length+1];
+        for(int n:map.keySet()){
+            int freq = map.get(n);
+            if(bucket[freq]==null)
+                bucket[freq] = new LinkedList<>();
+            bucket[freq].add(n);
+        }
+        
+        List<Integer> res = new LinkedList<>();
+        for(int i=bucket.length-1; i>0 && k>0; --i){
+            if(bucket[i]!=null){
+                List<Integer> list = bucket[i]; 
+                res.addAll(list);
+                k-= list.size();
+            }
+        }
+        
+        return res;
+    }
+}
